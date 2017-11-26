7d4ee220ba9e0d830429b94808a62529c1596994
Tania Pash <t.pashnyuk@gmail.com>
2017-11-26 11:28:56 +0200

    updated list page

--- src/pages/list/listController.js
+++ src/pages/list/listController.js
@@ -3,10 +3,9 @@ angular.module('rechi')
     var $ctrl = this;
     $ctrl.newItem = {
       name: " ",
-      description: " "
+      description: " ",
     };
-
-    console.log($ctrl.newItem);
+    $ctrl.itemCopy = [];
 
     $http.get('https://rechi.herokuapp.com/items')
       .then(function successCallback(response) {
@@ -16,12 +15,37 @@ angular.module('rechi')
       });
 
     console.log('List Controller', $ctrl);
-    
+
     $ctrl.saveItem = function () {
       $http.post('https://rechi.herokuapp.com/items', $ctrl.newItem)
         .then(function successCallback(response) {
           $ctrl.data.push(response.data);
-          $ctrl.newItem = {}
+          $ctrl.newItem = {};
+          console.log(response);
         }, function errorCallback(response) { console.log("Error2", response) })
     };
+
+    $ctrl.deleteItem = function (index) {
+      $http.delete('https://rechi.herokuapp.com/items/' + $ctrl.data[index].id)
+        .then(function successCallback(response) {
+          $ctrl.data.splice(index, 1);
+          console.log("Item was deleted!")
+        }, function errorCallback(response) { console.log("Error3: Item was not deleted!", response) })
+    };
+
+    $ctrl.saveUpdatedItem = function (index) {
+      $http.put('https://rechi.herokuapp.com/items/' + $ctrl.data[index].id, $ctrl.data[index])
+        .then(function successCallback(response) {
+          console.log("Item was udated!");
+        }, function errorCallback(response) { console.log("Error4", response) })
+    };
+
+    $ctrl.edit = function (item, index) {
+      $ctrl.itemCopy[index] = angular.copy(item);
+    }
+
+    $ctrl.cancel = function (index) {
+      $ctrl.data[index] = $ctrl.itemCopy[index];
+      console.log("the changes were Canceled!");
+    }
   });
