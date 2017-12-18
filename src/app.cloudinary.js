20faf12668845874299f78854ab7533f909759d1
Tania Pash <t.pashnyuk@gmail.com>
2017-12-18 23:39:08 +0200

    .

--- src/app.cloudinary.js
+++
@@ -1,8 +0,0 @@
-angular.module('rechi')
-.config(function (cloudinaryProvider) {
-    cloudinaryProvider.config({
-      upload_endpoint: 'https://api.cloudinary.com/v1_1/', // default 
-      cloud_name: 'tania', // required 
-      upload_preset: 'z6dgwlg5'// optional 
-    });
-  })
