d220ba7d5bb1c027d4ec5602b2fe09b69c52ad48
Tania Pash <t.pashnyuk@gmail.com>
2017-12-23 17:27:53 +0200

    updatets with authorization

--- src/services/authentification.js
+++ src/services/authentification.js
@@ -1,12 +1,25 @@
 angular.module('rechi')
-    .service('AuthenticationService', function ($http, $state) {
+    .service('AuthenticationService', function ($http, $state, $window) {
         this.checkLog = function (user) {
             $http.post('https://rechi.herokuapp.com/auth', user)
-            .then(function (response) {
-                $state.go('list');
-                console.log(response);
-            }, function (response) { console.log("LoginError", response) })
+                .then(function (response) {
+                  $state.go('list');
+                 $window.sessionStorage.token = response.data.token;
+               // add jwt token to auth header for all requests made by the $http service
+                 $http.defaults.headers.common.Authorization = 'Bearer ' + $window.sessionStorage.token;
+                   // $scope.message = 'Welcome';
+                    console.log(response, "LoggedIn");
+
+                }, function (response) {
+                    console.log("LoginError", response)
+                   // delete $window.sessionStorage.token;
+                   // $scope.message = 'Error: Invalid user or password';
+                })
         }
     });
 
 
+    // .success(function (data, status, headers, config) {
+    //     $window.sessionStorage.token = data.token;
+    //     $scope.message = 'Welcome';
+    //   })
