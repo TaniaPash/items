6b18c868b2b802aee659d751e8368324ac5d5879
Tania Pash <t.pashnyuk@gmail.com>
2017-12-24 11:35:49 +0200

    updated with auth and interceptor

--- src/directives/rchItem.js
+++ src/directives/rchItem.js
@@ -2,9 +2,13 @@ angular.module('rechi')
     .directive('rchItem', function () {
         return {
             restrict: 'E',
+            // controller: 'ListController',
+            // controllerAs: '$ctrl',
             scope: {
-                item: '='
-              },
+               item: '=',
+               $ctrl: '=',
+               $index: '='
+            },
             templateUrl: 'pages/list/rch-item.html'
             // to create isolate scope
         }
--- src/services/authentification.js
+++ src/services/authentification.js
@@ -3,23 +3,17 @@ angular.module('rechi')
         this.checkLog = function (user) {
             $http.post('https://rechi.herokuapp.com/auth', user)
                 .then(function (response) {
-                  $state.go('list');
-                 $window.sessionStorage.token = response.data.token;
-               // add jwt token to auth header for all requests made by the $http service
-                 $http.defaults.headers.common.Authorization = 'Bearer ' + $window.sessionStorage.token;
-                   // $scope.message = 'Welcome';
+                    $state.go('list');
+                    $window.sessionStorage.token = response.data.token;
+                    // add jwt token to auth header for all requests made by the $http service
+                   // $http.defaults.headers.common.Authorization = 'Bearer ' + $window.sessionStorage.token;
                     console.log(response, "LoggedIn");
-
                 }, function (response) {
                     console.log("LoginError", response)
-                   // delete $window.sessionStorage.token;
-                   // $scope.message = 'Error: Invalid user or password';
+                    delete $window.sessionStorage.token;
+                    console.log('Error: Invalid user or password');
                 })
         }
     });
 
 
-    // .success(function (data, status, headers, config) {
-    //     $window.sessionStorage.token = data.token;
-    //     $scope.message = 'Welcome';
-    //   })
