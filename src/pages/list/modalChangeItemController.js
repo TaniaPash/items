a9f483189b538a72a3b63d0940de771689cfb8a1
TaniaP <t.pashnyuk@gmail.com>
2018-02-07 18:00:50 +0200

    -

--- src/pages/list/modalChangeItemController.js
+++ src/pages/list/modalChangeItemController.js
@@ -3,20 +3,20 @@ angular.module('rechi')
 
 /*@ngInject*/
 
-function ModalChangeItemController ($http, itemCopy, $uibModal, $uibModalInstance, allConstants) {
-       
+function ModalChangeItemController($http, itemCopy, $uibModal, $uibModalInstance, allConstants) {
+
     var $ctrl = this;
 
-        $ctrl.item = itemCopy;
+    $ctrl.item = itemCopy;
 
-        $ctrl.save = function (item) {
-            $http.put( allConstants.apiHostUrl + '/items/' + $ctrl.item.id, $ctrl.item)
-                .then(function successCallback(response) {
-                    $uibModalInstance.close(response);
-                }, function errorCallback(response) { console.log("Error during PUT /Item", response) })
-        };
-
-        $ctrl.cancel = function () {
-            $uibModalInstance.dismiss('cancel');
-        };
+    $ctrl.save = function (item) {
+        $http.put(allConstants.apiHostUrl + '/items/' + $ctrl.item.id, $ctrl.item)
+            .then(function successCallback(response) {
+                $uibModalInstance.close(response);
+            }, function errorCallback(response) { console.log("Error during PUT /Item", response) })
     };
+
+    $ctrl.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+};
