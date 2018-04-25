af5792f479926cc1cce716b4f9d2805638792745
TaniaPash <t.pashnyuk@gmail.com>
2018-04-25 14:19:55 +0300

    fix: export var

--- scripts/semantic.sh
+++ scripts/semantic.sh
@@ -3,5 +3,4 @@ SEMANTIC_TAG=${1}
 echo "$@"
 echo $SEMANTIC_TAG
 echo "foo"
-
-sed -i -e "s/SEMANTIC_TAG/$SEMANTIC_TAG/" ./.travis.yml
+export SEM_TAG=$SEMANTIC_TAG
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SEMANTIC_TAG=${1} 
-echo $SEMANTIC_TAG
+SEM_TAG=${1} 
+echo $SEM_TAG
 echo "foo"
