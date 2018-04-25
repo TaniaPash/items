75ef3f8647816525c612850af0477a6d9cb5c777
TaniaPash <t.pashnyuk@gmail.com>
2018-04-25 12:53:59 +0300

    fix: test sem tag

--- scripts/semantic.sh
+++ scripts/semantic.sh
@@ -3,3 +3,5 @@ SEMANTIC_TAG=${1}
 echo "$@"
 echo $SEMANTIC_TAG
 echo "foo"
+
+sed -i -e "s/SEMANTIC_TAG/$SEMANTIC_TAG/" ./.travis.yml
---
+++ scripts/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SEMANTIC_TAG=${1} 
+echo $SEMANTIC_TAG
+echo "foo"
