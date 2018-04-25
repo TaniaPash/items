5464bd4dab650fe55899a02efb1923e42711eaef
TaniaPash <t.pashnyuk@gmail.com>
2018-04-25 23:16:24 +0300

    export var with shell script

--- scripts/semantic.sh
+++ scripts/semantic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SEMANTIC_TAG=${1}
-echo "$@"
-echo "export SEM_TAG=$@"
-export SEM_TAG=$SEMANTIC_TAG
-export SEMAN_TAG=$@
+#SEMANTIC_TAG=${1}
+#echo "$@"
+#echo "export SEM_TAG=$@"
+export SEM_TAG=oidtsj;k
+#export SEMAN_TAG=$@
